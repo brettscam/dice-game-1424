617b84965df573cdc55d85369f8cb585a43bf7aa
brettscam <brettcameronsmith@gmail.com>
2024-11-12 18:41:04 -0800

    Delete src/components/app directory

--- src/components/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { GameLobby } from '@/components/game/GameLobby';
-
-export default function HomePage() {
-  return (
-    <div className="container mx-auto py-8">
-      <GameLobby />
-    </div>
-  );
-}
