f1316339081c579066c20f5722a53e325ae7335d
brettscam <brettcameronsmith@gmail.com>
2024-11-12 18:20:41 -0800

    Create page.tsx

---
+++ src/components/app/page.tsx
@@ -0,0 +1,9 @@
+import { GameLobby } from '@/components/game/GameLobby';
+
+export default function HomePage() {
+  return (
+    <div className="container mx-auto py-8">
+      <GameLobby />
+    </div>
+  );
+}
